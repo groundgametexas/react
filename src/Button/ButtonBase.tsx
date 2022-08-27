195215e6792779a545381b3d6a7d43843d3e3542
Matt Oliver <mattoliver.mattoliver@gmail.com>
2022-08-26 22:49:54 -0500

    scaffold button and input

--- src/Button/ButtonBase.tsx
+++ src/Button/ButtonBase.tsx
@@ -4,6 +4,9 @@ import React, {
   HTMLAttributes,
   ComponentPropsWithRef,
 } from "react";
+import {Box} from '..'
+import type * as Polymorphic from "@radix-ui/react-polymorphic";
+
 
 export type ButtonBaseProps = {
   /** The content to display in the button. */
@@ -17,6 +20,14 @@ export type ButtonBaseProps = {
    * @default 'button'
    */
   type?: "button" | "submit" | "reset";
+  /** Show loading button style and spinner. */
+  isLoading?: boolean;
+  /** A helper rendered at the start of the button. */
+  leadingVisual?: () => React.ReactNode;
+  /** A helper rendered at the end of the button. */
+  trailingVisual?: () => React.ReactNode;
+  /** A helper rendered as a loading state. */
+  loadingEnhancer?: () => React.ReactNode;
 };
 
 /**
@@ -34,6 +45,90 @@ export const ButtonBase = React.forwardRef<HTMLButtonElement, ButtonBaseProps>(
 
 ButtonBase.displayName = "ButtonBase";
 
+/* -------------------------------------------------------------------------------------------------
+ * ButtonContent
+ * -----------------------------------------------------------------------------------------------*/
+export type ButtonContentProps = Pick<
+  ButtonBaseProps,
+  "leadingVisual" | "trailingVisual" | "isLoading" | "loadingEnhancer"
+>;
+
+const BUTTON_CONTENT_NAME = "ButtonContent";
+const BUTTON_CONTENT_DEFAULT_TAG = "span";
+
+export type ButtonContentPrimitive = Polymorphic.ForwardRefComponent<
+  typeof BUTTON_CONTENT_DEFAULT_TAG,
+  ButtonContentProps
+>;
+
+/**
+ * `ButtonContent` contains the content of a `Button`.
+ */
+export const ButtonContent = React.forwardRef((props, forwardedRef) => {
+  const {
+    leadingVisual,
+    trailingVisual,
+    loadingEnhancer,
+    isLoading,
+    children,
+  } = props;
+
+  return (
+    <React.Fragment>
+      {isLoading ? null : leadingVisual && leadingVisual()}
+
+      {children}
+
+      {isLoading ? null : trailingVisual && trailingVisual()}
+    </React.Fragment>
+  );
+}) as ButtonContentPrimitive;
+
+ButtonContent.displayName = BUTTON_CONTENT_NAME;
+
+
+/* -------------------------------------------------------------------------------------------------
+ * ButtonLoadingContainer
+ * -----------------------------------------------------------------------------------------------*/
+export type ButtonLoadingContainerProps = Pick<ButtonBaseProps, "isLoading">;
+
+const BUTTON_LOADING_CONTAINER_NAME = "ButtonLoadingContainer";
+const BUTTON_LOADING_CONTAINER_DEFAULT_TAG = "span";
+
+export type ButtonLoadingContainerPrimitive = Polymorphic.ForwardRefComponent<
+  typeof BUTTON_LOADING_CONTAINER_DEFAULT_TAG,
+  ButtonLoadingContainerProps
+>;
+
+/**
+ * `ButtonLoadingContainer` handles animations for any a `loadingEnhancer` used in `ButtonContent`.
+ */
+export const ButtonLoadingContainer = React.forwardRef(
+  (props, forwardedRef) => {
+    const {
+      as = BUTTON_LOADING_CONTAINER_DEFAULT_TAG,
+      isLoading,
+      children,
+      ...buttonLoadingContainerProps
+    } = props;
+
+    return isLoading ? (
+      <Box
+        {...buttonLoadingContainerProps}
+        as={as}
+        ref={forwardedRef}
+        data-loading={isLoading ? "true" : "false"}
+      >
+        {children}
+      </Box>
+    ) : null;
+  }
+) as ButtonLoadingContainerPrimitive;
+
+ButtonLoadingContainer.displayName = BUTTON_LOADING_CONTAINER_NAME;
+
 export default {
-  ButtonBase: ButtonBase,
+  Base: ButtonBase,
+  Content: ButtonContent,
+  LoadingContainer: ButtonLoadingContainer
 };
---
+++ src/Button/types.ts
@@ -0,0 +1,6 @@
+export type VariantType =
+  | "default"
+  | "primary"
+  | "secondary"
+  | "danger"
+  | "outline";

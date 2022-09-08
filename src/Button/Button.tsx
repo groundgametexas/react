98d2c08175c60e3f28e916d67ff176dcaacd3fa0
Matt Oliver <mattoliver.mattoliver@gmail.com>
2022-09-07 22:38:01 -0500

    wip

--- src/Button/Button.tsx
+++ src/Button/Button.tsx
@@ -1,5 +1,10 @@
 import React, { ComponentProps } from "react";
+import type * as Stitches from '@stitches/react';
+import { createStitches } from '@stitches/react';
+
 import { ButtonBase, ButtonBaseProps } from "./ButtonBase";
+import { styled } from "../stitches.config";
+
 
 /**
  * Buttons consist of a clickable area usually containing a textual label or an icon that users can click to perform an action.
@@ -12,8 +17,13 @@ import { ButtonBase, ButtonBaseProps } from "./ButtonBase";
 const Button = ({ ...props }: ButtonBaseProps) => {
   let { children, ...additionalProps } = props;
 
-  return <ButtonBase {...additionalProps}>{children}</ButtonBase>;
+  return <StyledButtonBase {...additionalProps}>{children}</StyledButtonBase>;
 };
+
+const StyledButtonBase = styled(ButtonBase, {
+  color: 'white',
+  backgroundColor: '$backgroundAccentOrangeBolder'
+})
 
 export type ButtonProps = ComponentProps<typeof Button>;
 export default Button;

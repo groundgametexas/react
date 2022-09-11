d0f9f648013cc25729907686ca2ab0e00b729ca6
Matt Oliver <mattoliver.mattoliver@gmail.com>
2022-09-11 14:47:16 -0500

    scaffold input and label

--- src/Input/Input.tsx
+++ src/Input/Input.tsx
@@ -1,23 +1,38 @@
 import React, { ForwardRefRenderFunction, InputHTMLAttributes } from "react";
+import { styled } from "../stitches.config";
 
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
-  name: string;
-  label: string;
-  ref: string;
+  name?: string;
+  label?: string;
+  ref?: string;
 }
 
 /**
  * Input component
  */
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
-  ({ name, label, ...delegated }, ref) => {
+  ({ name, ...delegated }, ref) => {
     return (
-      <label>
-        {label}
-        <input {...delegated} name={name} ref={ref} />
-      </label>
+      <StyledInput
+        {...delegated}
+        name={name}
+        ref={ref}
+        className={`text-base shadow-sm focus:ring-blue-500 focus:border-blue-500`}
+      />
     );
   }
 );
+
+const StyledInput = styled("input", {
+  display: "block",
+  border: "1px solid",
+  borderColor: "$borderDefault",
+  width: "100%",
+  paddingTop: "$2",
+  paddingBottom: "$2",
+  borderRadius: "$3",
+  fontSize: "1rem",
+  lineHeight: "1.5rem",
+});
 
 export default Input;
